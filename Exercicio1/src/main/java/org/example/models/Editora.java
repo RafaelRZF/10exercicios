11148e4378aae85442e8c0f9aacfba935151446c
Vinicius Almeida <viniciusalmeidadeavila@gmail.com>
2026-06-09 21:44:36 -0300

    feat: fazendo o exercicio1

---
+++ Exercicio1/src/main/java/org/example/models/Editora.java
@@ -0,0 +1,36 @@
+package org.example.models;
+
+public class Editora {
+    private int id;
+    private String nome;
+    private String cidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Editora(String nome, String cidade){
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+}
---
+++ Exercicio1/src/main/java/org/example/models/Livro.java
@@ -0,0 +1,56 @@
+package org.example.models;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private int anoPublicacao;
+    private Autor autor_id;
+    private Editora editora_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public Autor getAutor_id() {
+        return autor_id;
+    }
+
+    public Editora getEditora_id() {
+        return editora_id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setAutor_id(Autor autor_id) {
+        this.autor_id = autor_id;
+    }
+
+    public void setEditora_id(Editora editora_id) {
+        this.editora_id = editora_id;
+    }
+
+    public Livro(String titulo, int anoPublicacao, Autor autor_id, Editora editora_id){
+        this.titulo = titulo;
+        this.anoPublicacao = anoPublicacao;
+        this.autor_id = autor_id;
+        this.editora_id = editora_id;
+    }
+}
